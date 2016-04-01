b9ef96df4ff48992960a876a5fd567a73184bac8
Luke Carrier <luke@carrier.im>
2016-04-01 11:30:41 +0100

    Plugin boilerplate

---
+++ lang/en/local_signin.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Enhanced authentication.
+ *
+ * @author Luke Carrier <luke.carrier@floream.com>
+ * @copyright 2016 Floream Limited
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+// Component metadata
+$string['pluginname'] = 'Enhanced authentication';
---
+++ version.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Enhanced authentication.
+ *
+ * @author Luke Carrier <luke.carrier@floream.com>
+ * @copyright 2016 Floream Limited
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+$plugin->component = 'local_signin';
+
+$plugin->version  = 2016040100;
+$plugin->maturity = MATURITY_ALPHA;
