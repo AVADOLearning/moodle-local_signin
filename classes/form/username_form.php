5541a4139c6855d9cd3138699d53d00d08a42665
Earle Skinner <earle.skinner@avadolearning.com>
2017-10-16 11:04:21 +0100

    Stubs for username and password forms

---
+++ classes/form/username_form.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Username form
+ *
+ * @author Earle Skinner <earle.skinner@avadolearning.com>
+ * @copyright 2017 AVADO Learning
+ */
+
+namespace local_signin\form;
+
+use coding_exception;
+use local_signin\util;
+use moodleform;
+
+defined('MOODLE_INTERNAL') || die;
+
+require_once "{$CFG->libdir}/formslib.php";
+
+public class username_form extends moodleform {
+    /**
+     * @override \moodleform
+     */
+    public function definition() {
+
+    }
+}
